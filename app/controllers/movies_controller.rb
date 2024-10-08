6e3312851f3a032c882de2abdb599f677327f76e
sarkriti <sarkriti@tamu.edu>
2024-10-08 16:01:11 -0500

    Sort links

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -3,7 +3,7 @@ class MoviesController < ApplicationController
 
   # GET /movies or /movies.json
   def index
-    @movies = Movie.all
+    Movie.order("#{params[:sort]} DESC")
   end
 
   # GET /movies/1 or /movies/1.json
