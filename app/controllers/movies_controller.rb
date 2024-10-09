560c60993685d0bd7efbd4373e00b8793846f7bf
sarkriti <sarkriti@tamu.edu>
2024-10-09 11:49:42 -0500

    Table sort update

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,9 +1,9 @@
 class MoviesController < ApplicationController
   before_action :set_movie, only: %i[ show edit update destroy ]
-
+  helper_method :sort_column, :sort_direction 
   # GET /movies or /movies.json
   def index
-    @movies = Movie.order(params[:sort])
+    @movies = Movie.order(sort_column + " " + sort_direction) 
   end
 
   # GET /movies/1 or /movies/1.json
@@ -19,6 +19,8 @@ class MoviesController < ApplicationController
   def edit
   end
 
+  def sort_column
+    Movie.column_names.include?(params[:sort]) ? params[:sort] : "title"
   # POST /movies or /movies.json
   def create
     @movie = Movie.new(movie_params)
@@ -66,6 +68,13 @@ class MoviesController < ApplicationController
     # Only allow a list of trusted parameters through.
     def movie_params
       params.require(:movie).permit(:title, :rating, :description, :release_date)
+    end
+
+    def sort_column
+      Movie.column_names.include?(params[:sort]) ? params[:sort] : "name"
+    end
+    def sort_direction
+       %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
     end
 end
 #sort movies table by column
