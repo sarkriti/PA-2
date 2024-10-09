abd9298264fe3c3713c6f6e5f2cd8834c435ff15
sarkriti <sarkriti@tamu.edu>
2024-10-09 16:30:18 -0500

    revert

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,7 @@
 module ApplicationHelper
     def sortable(column, title = nil)
         title ||= column.titleize
-        css_class = column == sort_column ? "hilight" : nil 
         direction = column == params[:sort] && params[:direction] == "asc" ? "desc" : "asc"
-        link_to title, {:sort => column, :direction => direction}, {:class => css_class}
+        link_to title, {:sort => column, :direction => direction}
     end
 end
