0fcca58818866867b93986d12ea945db4f81a876
sarkriti <sarkriti@tamu.edu>
2024-10-09 23:23:27 -0500

    arrow3

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,6 +4,6 @@ module ApplicationHelper
         css_class = column == sort_column ? "hilight" : nil
         css_class_symbol = column == sort_column ? "#{sort_direction}" : nil
         direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
-        link_to title, {:sort => column, :direction => direction},{:class => css_class},{:class => css_class_symbol}
+        link_to title, {:sort => column, :direction => direction},{:class => css_class}
     end
 end
