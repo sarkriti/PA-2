ae043c1d6592257ca97b0519cf04703ccc25ff7f
sarkriti <sarkriti@tamu.edu>
2024-10-09 22:10:07 -0500

    sort symbol3

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
     def sortable(column, title = nil)
         title ||= column.titleize
         css_class = column == params[:sort] ? "current #{sort_direction}" : nil
-        direction = column == sort_column && sort_column == "asc" ? "desc" : "asc"
+        direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
         link_to title, {:sort => column, :direction => direction},{:class => css_class}
     end
 end
